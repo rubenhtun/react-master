c6af66484d3f60b91a743fb2e532153617d55cbd
Ruben Htun <rubenhtun2003@gmail.com>
2025-05-20 15:54:52 +0700

    feat: learned context

--- React-Oldie/src/App.js
+++ React-Oldie/src/App.js
@@ -1,61 +1,17 @@
-import React from "react";
-
-class Item extends React.Component {
-  render() {
-    return (
-      <li>
-        {this.props.name}, ${this.props.price}
-      </li>
-    );
-  }
-}
-
-class AddForm extends React.Component {
-  add = () => {
-    let name = this.nameRef.current.value;
-    let price = this.priceRef.current.value;
-    this.props.add(name, price);
-  };
-
-  render() {
-    return (
-      <div>
-        <input type="text" ref={this.nameRef} /> <br />
-        <input type="text" ref={this.priceRef} /> <br />
-        <button onClick={this.add}>Add</button>
-      </div>
-    );
-  }
-}
-
+const MyContext = React.createContext("Hello React");
 class App extends React.Component {
-  state = {
-    items: [
-      { id: 1, name: "Apple", price: 0.99 },
-      { id: 2, name: "Orange", price: 0.89 },
-    ],
-  };
-
-  add = (name, price) => {
-    let id = this.state.items.length + 1;
-
-    this.setState({
-      items: [...this.state.items, { id, name, price }],
-    });
-  };
-
   render() {
-    return (
-      <div>
-        <h1>Hello React</h1>
-        <ul>
-          {this.state.items.map((i) => {
-            return <Item key={i.id} name={i.name} price={i.price} />;
-          })}
-        </ul>
-        <AddForm ref={this.add} />
-      </div>
-    );
+    return <Header />;
   }
 }
-export default App;
+class Header extends React.Component {
+  render() {
+    return <Title />;
+  }
+}
+class Title extends React.Component {
+  static contextType = MyContext;
+  render() {
+    return <h1>{this.context}</h1>;
+  }
+}
